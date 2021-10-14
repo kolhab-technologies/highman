a2220c9bb5c8b68311891458b6fe4b4ec518cba6
Abdulwahab Ashimi <princeadebowale3@gmail.com>
2021-10-14 19:05:55 +0100

    awesome

--- header.php
+++ header.php
@@ -92,16 +92,16 @@
 				</div>
 			</div>
 			</nav>
-		<nav id="site-navigation" class="main-navigation">
+		<!--nav id="site-navigation" class="main-navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'highman' ); ?></button>
 			<?php
-			wp_nav_menu(
+			/*wp_nav_menu(
 				array(
 					'theme_location' => 'menu-1',
 					'menu_id'        => 'primary-menu',
 				)
-			);
+			);*/
 			?>
 
-		</nav><!-- #site-navigation -->
+		</nav--><!-- #site-navigation -->
 	</header><!-- #masthead -->
