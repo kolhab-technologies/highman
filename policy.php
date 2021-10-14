3a4c2e979a27ae16c2b55fcabf1dafeaf9062b8f
Abdulwahab Ashimi <princeadebowale3@gmail.com>
2021-10-14 20:49:43 +0100

    policy updated

--- policy.php
+++ policy.php
@@ -1,12 +1,14 @@
 <?php
 /**
- * The main template file
+ * Template Name:  * The template for displaying Privacy Policy
  *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
+ * The template for displaying Privacy Policy
  *
+ * This is the template that displays the area of the page that contains both the Privacy
+ * Policy
+ * 
+ * 
+ * 
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
  * @package Highman
